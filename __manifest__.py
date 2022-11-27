9d1a510fdf7311452b111f73c346e97e5c47d0ce
shortcut1337 <really_mad@hotmail.com>
2022-11-27 23:26:26 +0100

    [IMP] add warehouse selection to ecommerce
    - Possible to see stock in other warehouse
    - Can select a different one
    - Hint to mention than during checkout the quantities of already added
      product might be altered (eg if come from another warehouse with less
      or no qty) -> Note that Odoo already has something that will hint the
      user that we reduced qty on payment page (orange warning icon)

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gse_website_sale_stock",
+    'summary': """Customizations de l'ecommerce pour Goshop Energy""",
+    'description': """""",
+    'author': "Sébastien Bühl",
+    'website': "http://www.buhl.be",
+    'category': 'Customizations',
+    'version': '0.1',
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'website_sale_stock',
+    ],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'gse_custo/static/src/js/variant_mixin.js',
+        ],
+    },
+}
